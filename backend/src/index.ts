4c60e72aef0b4ca00cbb2a2cf313923ead403814
amasha <fernandothushini8@gmail.com>
2024-07-06 15:48:20 +0530

    Create Routes & Middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,4 +2,31 @@ import express from "express";
 
 const app = express();
 
+// routes
+app.use(express.json());
+
+app.get("/hello", (req, res, next) => {
+  return res.send("Hello!");
+});
+
+app.post("/hello", (req, res, next) => {
+  console.log(req.body.name);
+  return res.send("Hello!");
+});
+
+app.put("/hello", (req, res, next) => {
+  console.log(req.body.name);
+  return res.send("Hello!");
+});
+
+app.delete("/hello", (req, res, next) => {
+  console.log(req.body.name);
+  return res.send("Hello!");
+});
+
+app.delete("/user/:userId", (req, res, next) => {
+  console.log(req.params.userId);
+  return res.send("Hello!");
+});
+
 app.listen(5000, () => console.log("Server Open on 5000"));
